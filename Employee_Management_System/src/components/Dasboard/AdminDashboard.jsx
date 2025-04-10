78c15af3fbd5808439a6b001cb5038a93d80554b
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-04-10 22:05:29 +0530

    10/04/2025
    Admin Dashboard UI

--- Employee_Management_System/src/components/Dasboard/AdminDashboard.jsx
+++ Employee_Management_System/src/components/Dasboard/AdminDashboard.jsx
@@ -6,32 +6,37 @@ function AdminDashboard() {
     <div className='h-screen w-full p-10'>
       < Header />
 
-      <div>
+      <div className='p-5 bg-[#1c1c1c] mt-7 rounded '>
 
-        <form className='flex items-start justify-between'>
-          <div>
-            <h3>
-              Task Title
-            </h3>
-            <input type="text" placeholder='Enter your task' />
+        <form className='flex  flex-wrap w-full   items-start justify-between'>
+
+          <div className='w-1/2'>
+            <div >
+              <h3 className='text-sm text-gray-300 mb-0.5'>
+                Task Title
+              </h3>
+              <input  type="text" placeholder='Enter your task' />
+            </div>
+            <div>
+              <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
+              <input type="date" />
+            </div>
+            <div>
+              <h3 className='text-sm text-gray-300 mb-0.5' >Assign to</h3>
+              <input type="text" placeholder='Employee Name' />
+            </div>
+            <div>
+              <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
+              <input type="text" placeholder='design, dev,etc' />
+            </div>
           </div>
-          <div>
-            <h3>Description</h3>
+
+          <div className='w-1/2'>
+            <h3  className='text-sm text-gray-300 mb-0.5'>Description</h3>
             <textarea name="" id="" cols="30" rows="10"></textarea>
           </div>
-          <div>
-            <h3>Date</h3>
-            <input type="date" />
-          </div>
-          <div>
-            <h3>Assign to</h3>
-            <input type="text" placeholder='Employee Name' />
-          </div>
-          <div>
-            <h3>Category</h3>
-            <input type="text" placeholder='design, dev,etc' />
-          </div>
           <button>Create Task</button>
+
         </form>
       </div>
     </div>
