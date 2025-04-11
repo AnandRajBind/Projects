0a98e46a2dc1aaced409d6d646299f41b471f764
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-04-11 23:43:13 +0530

    11/04/2025
    Admin DashBoard UI  Completed

--- Employee_Management_System/src/components/Dasboard/AdminDashboard.jsx
+++ Employee_Management_System/src/components/Dasboard/AdminDashboard.jsx
@@ -1,44 +1,12 @@
 import React from 'react'
 import Header from '../others/Header'
+import CreateTask from '../others/CreateTask';
 
 function AdminDashboard() {
   return (
     <div className='h-screen w-full p-10'>
       < Header />
-
-      <div className='p-5 bg-[#1c1c1c] mt-7 rounded '>
-
-        <form className='flex  flex-wrap w-full   items-start justify-between'>
-
-          <div className='w-1/2'>
-            <div >
-              <h3 className='text-sm text-gray-300 mb-0.5'>
-                Task Title
-              </h3>
-              <input  type="text" placeholder='Enter your task' />
-            </div>
-            <div>
-              <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
-              <input type="date" />
-            </div>
-            <div>
-              <h3 className='text-sm text-gray-300 mb-0.5' >Assign to</h3>
-              <input type="text" placeholder='Employee Name' />
-            </div>
-            <div>
-              <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
-              <input type="text" placeholder='design, dev,etc' />
-            </div>
-          </div>
-
-          <div className='w-1/2'>
-            <h3  className='text-sm text-gray-300 mb-0.5'>Description</h3>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-          </div>
-          <button>Create Task</button>
-
-        </form>
-      </div>
+      <CreateTask />
     </div>
   )
 }
