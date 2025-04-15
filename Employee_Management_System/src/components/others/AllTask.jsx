210d94ce77e735b9872b53e05d23474ac6a9bfaf
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-04-15 10:53:57 +0530

    15/04/2025
    passing Data in tasks

--- Employee_Management_System/src/components/others/AllTask.jsx
+++ Employee_Management_System/src/components/others/AllTask.jsx
@@ -7,24 +7,22 @@ function AllTask() {
     console.log(authData.employees);
 
     return (
-        <div id='allTask' className='bg-[#1c1c1c] p-5 rounded  mt-5   h-48 overflow-auto'>
-           
+        <div id='allTask' className='bg-[#1c1c1c] p-5 rounded  mt-5   '>
            <div className='bg-red-400  mb-2  py-2 px-4 flex justify-between rounded'>
-                    <h2 className='w-1/5 bg-red-600'>Employee Name </h2>
-                    <h3 className='w-1/5 bg-red-600'>New Task</h3>
-                    <h5 className='w-1/5 bg-red-600'>Active Task</h5>
-                    <h5 className='w-1/5 bg-red-600'>Completed</h5>
-                    <h5 className='w-1/5 bg-red-600'>Failed</h5>
-                </div>
-           
-          <div className='h-[80%]'>
+                    <h2 className=' text-lg font-medium w-1/5 '>Employee Name </h2>
+                    <h3 className=' text-lg font-medium w-1/5 '>New Task</h3>
+                    <h5 className=' text-lg font-medium w-1/5 '>Active Task</h5>
+                    <h5 className=' text-lg font-medium w-1/5 '>Completed</h5>
+                    <h5 className=' text-lg font-medium w-1/5 '>Failed</h5>
+</div>
+          <div className=''>
           {authData.employees.map(function (elem) {
-                return <div className='bg-red-400  mb-2  py-2 px-4 flex justify-between rounded'>
-                    <h2 className='w-1/5 bg-red-600'>{elem.firstName} </h2>
-                    <h3 className='w-1/5 bg-red-600'>Task</h3>
-                    <h5 className='w-1/5 bg-red-600'>Status</h5>
-                    <h5 className='w-1/5 bg-red-600'>Status</h5>
-                    <h5 className='w-1/5 bg-red-600'>Status</h5>
+                return <div className=' border-2 border-emerald-400  mb-2  py-2 px-4 flex justify-between rounded '>
+                    <h2 className=' text-lg font-medium w-1/5 '>{elem.firstName} </h2>
+                    <h3 className=' text-lg font-medium w-1/5 text-blue-600'>{elem.taskCounts.newTask}</h3>
+                    <h5 className=' text-lg font-medium w-1/5  text-yellow-400'>{elem.taskCounts.active}</h5>
+                    <h5 className=' text-lg font-medium w-1/5 text-green-600'>{elem.taskCounts.completed}</h5>
+                    <h5 className=' text-lg font-medium w-1/5 text-pink-800'>{elem.taskCounts.failed}</h5>
                 </div>
             })}
           </div>
